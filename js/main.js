6b41bc37f851e805957c8455ebbb37a18714655b
ankjai <ankit.jaiswal@appdirect.com>
2015-08-24 15:24:53 -0700

    Fixing drawGameScreen() to reflect correctly as its expected.

--- js/main.js
+++ js/main.js
@@ -37,20 +37,30 @@ function moveObjectsAndEnemies() {
 function drawGameScreen(ctx) {
     console.log("drawGameScreen...")
 
-    var gameScreen = new Image();
+    var water = new Image();
+    water.src = "images/water-block.png";
 
-    var grassSrc = "images/grass-block.png";
-    var stoneSrc = "images/stone-block.png";
-    var waterSrc = "images/water-block.png";
+    var stone = new Image();
+    stone.src = "images/stone-block.png";
+
+    var grass = new Image();
+    grass.src = "images/grass-block.png";
 
     for (var dy = 0; dy < 6; dy++) {
         for (var dx = 0; dx < 5; dx++) {
-        	gameScreen.src = grassSrc;
-            ctx.drawImage(gameScreen, (dx * 101), (dy * 80));
+            if (dy == 0) {
+                ctx.drawImage(water, (dx * 101), (dy * 80));
+            } else if (dy > 0 && dy < 4) {
+                ctx.drawImage(stone, (dx * 101), (dy * 80));
+            } else if (dy >= 4) {
+                ctx.drawImage(grass, (dx * 101), (dy * 80));
+            };
+            // gameScreen.onload = function() {
+            //     // body...
+            //     ctx.drawImage(gameScreen, (dx * 101), (dy * 80));
+            // }
         }
     }
-
-    ctx.restore();
 }
 
 init();
